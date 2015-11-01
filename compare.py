e5d8bb7e5da733734bfa05b3f1a3fa9087048258
rishirajsurti <rishirajsurti.iitm@gmail.com>
2015-11-01 21:09:58 +0530

    completed code for comparing output files

--- compare.py
+++ compare.py
@@ -10,13 +10,34 @@ import glob
 import numpy 
 import scipy
 import csv
+from sklearn.metrics import accuracy_score
+from collections import Counter
 os.chdir('/home/rishiraj/cs5011/contest/ml_contest/');
 #%%
-
-files = ['/bayes/bayes_rs_output.csv', '/svm/svm_rs_output.csv'] # add your files here;
+def most_common(lst):
+    return max(set(lst), key=lst.count);
+    
+files = ['svm/svm_rs_output.csv', 'bayes/bayes_rs_output.csv'] # add your files here;
 
 test_Y=[]
-for line in open('../../test_data_Y.csv').readlines():
+for line in open('../test_data_Y.csv').readlines():
     test_Y.append(map(float,line.strip().split(" "))[0]);
 
 final = np.zeros(len(test_Y));
+outputs=[];
+
+for i in xrange(len(files)):
+    test_Y=[]
+    for line in open(files[i]).readlines():
+        test_Y.append(map(float,line.strip().split(" "))[0]);
+    outputs.append(test_Y);    
+    
+for i in xrange(len(final)):
+    op=[];
+    for j in xrange(len(outputs)):
+        op.append(outputs[j][i]); #j'th file, i'th element
+    final[i] = most_common(op); # need more files
+    
+
+accuracy_score(test_Y, final)
+#0.91660851360781581
