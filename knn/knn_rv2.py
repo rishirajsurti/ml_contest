59ef914146bcb89213164e8ec29d207531361166
Rishiraj Surti <rishirajsurti.iitm@gmail.com>
2015-11-15 20:25:03 +0530

    Create knn_rv2.py

---
+++ knn/knn_rv2.py
@@ -0,0 +1,45 @@
+# coding: utf-8
+from sklearn import neighbors
+from sklearn.metrics import accuracy_score
+import numpy as np
+
+print 'Loading train data...'
+X = np.loadtxt('../../train_X.csv', delimiter=',')
+y = np.loadtxt('../../train_Y.csv', dtype=int, delimiter=',')
+# X = np.loadtxt('../../train_data_X.csv', delimiter=' ')
+# y = np.loadtxt('../../train_data_Y.csv', dtype=int, delimiter=' ')
+
+print 'Normalising train data...'
+for i in range(len(X.T)):
+	mean = np.mean(X[:, i])
+	if mean != 0:
+		variance = np.mean((X[:, i] - mean)**2)
+		X[:, i] = (X[:, i] - mean)/np.sqrt(variance)
+
+print 'Loading test data...'
+testData_X = np.loadtxt('../../test_X.csv', delimiter=',')
+# testData_X = np.loadtxt('../../test_data_X.csv', delimiter=' ')
+
+print 'Normalising test data...'
+for i in range(len(testData_X.T)):
+	mean = np.mean(testData_X[:, i])
+	if mean != 0:
+		variance = np.mean((testData_X[:, i] - mean)**2)
+		testData_X[:, i] = (testData_X[:, i] - mean)/np.sqrt(variance)
+
+# testData_Y = np.loadtxt('../../test_data_Y.csv', delimiter=' ')
+
+# For the given training dataset, after splitting it into
+# 80 p.c. training and 20 p.c. test sets, we obtained k = 7
+# gives the highest accuracy between k = 5 to 15 considering
+# the problem of computations for higher value of k
+
+n = 7
+print 'Performing kNN classification with k = %d...' % (n)
+clf = neighbors.KNeighborsClassifier(n, weights='uniform')
+clf.fit(X, y)
+labelsPredicted = clf.predict(testData_X)
+# print "%d : %f" % (n, accuracy_score(testData_Y, labelsPredicted))
+np.savetxt("%dnn_output_rv.csv" % (n), labelsPredicted, fmt="%.0f", delimiter='\n')
+
+print 'Done'
